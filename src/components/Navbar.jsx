e9212657145f0f6cc577cb5e98d02ee76baa07cb
RYUSNEY <ruapazap@est.unap.edu.pe>
2025-06-03 10:23:02 -0500

    Boton para cambio de idioma

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,44 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo-removebg-preview.png";
-import { FaAddressBook, FaHome } from "react-icons/fa";
+import { FaAddressBook, FaHome, FaGlobe } from "react-icons/fa";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+
 
 export default function Navbar() {
-    return (
-      <nav className="bg-[#2bccd4] shadow-md">
-        <div className="container mx-auto flex items-center justify-between p-4">
-          <Link to="/" className="text-2xl font-bold text-green-600 flex items-center gap-2">
-            <img src={logo} alt="Logo" className="h-8 w-8" />
-            Voluntariado
+  const { i18n, t } = useTranslation();
+  const [language, setLanguage] = useState(i18n.language || "es");
+
+  const toggleLanguage = () => {
+    const newLang = language === "es" ? "en" : "es";
+    i18n.changeLanguage(newLang);
+    setLanguage(newLang);
+  };
+
+  return (
+    <nav className="bg-[#2bccd4] shadow-md">
+      <div className="container mx-auto flex items-center justify-between p-4">
+        <Link to="/" className="text-2xl font-bold text-green-600 flex items-center gap-2">
+          <img src={logo} alt="Logo" className="h-8 w-8" />
+          Voluntariado
+        </Link>
+        <div className="space-x-6 flex items-center">
+          <Link to="/" className="hover:text-green-500 flex items-center gap-2">
+            Inicio
+            <FaHome className="text-xl" />
           </Link>
-          <div className="space-x-6 flex items-center">
-            <Link to="/" className="hover:text-green-500 flex items-center gap-2">
-              Inicio
-              <FaHome className="text-xl" />
-            </Link>
-            <Link to="/contacto" className="hover:text-green-500 flex items-center gap-2 align-middle">
-              Contacto
-              <FaAddressBook className="text-xl" />
-            </Link>
-          </div>
+          <Link to="/contacto" className="hover:text-green-500 flex items-center gap-2 align-middle">
+            Contacto
+            <FaAddressBook className="text-xl" />
+          </Link>
+          <button onClick={toggleLanguage} className="text-white bg-green-600 px-3 py-1 rounded hover:bg-green-700 flex items-center gap-2">
+            <FaGlobe className="text-xl" />
+            {language.toUpperCase()}
+          </button>
         </div>
-      </nav>
-    );
-  }
+        
+      </div>
+    </nav>
+  );
+}
 
