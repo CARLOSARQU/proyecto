4f18f537c29bd31d30ea3c786d3340c0c5c793d0
RYUSNEY <ruapazap@est.unap.edu.pe>
2025-06-19 11:06:44 -0500

    Seccion Areas terminada

--- src/sections/HomeSections/ProjectsSection.jsx
+++ src/sections/HomeSections/ProjectsSection.jsx
@@ -1,13 +1,16 @@
 import { useTranslation } from 'react-i18next';
-import iaImg from '../../assets/img/ia.webp';
-import logoImg from '../../assets/img/logo.webp';
+import desarrollo_soporte from '../../assets/img/areas/desarrollo_soporte.png';
+import social_media       from '../../assets/img/areas/social_media.png';
+import programa_educativo from '../../assets/img/areas/programa_educativo.png';
+import talento_humano     from '../../assets/img/areas/talento_humano.png';
+import logistica          from '../../assets/img/areas/logistica.png';
 
 const imageMap = {
-  a: iaImg,
-  b: logoImg,
-  c: iaImg,
-  d: logoImg,
-  e: iaImg,
+  a: desarrollo_soporte,
+  b: social_media,
+  c: programa_educativo,
+  d: talento_humano,
+  e: logistica,
 };
 
 const avatarMap = {
@@ -31,7 +34,7 @@ export default function ProjectsSection() {
             {t('projects.subtitle')}
           </p>
         </div>
-        <div className="mx-auto mt-16 grid max-w-none grid-cols-1 gap-x-8 gap-y-20 sm:grid-cols-2 lg:grid-cols-5">
+        <div className="mx-auto mt-8 grid max-w-none grid-cols-1 gap-x-8 gap-y-20 sm:grid-cols-2 lg:grid-cols-5">
             {postKeys.map((key) => (
               <article key={key} className="flex flex-col">
                 <div className="relative w-full">
@@ -44,7 +47,7 @@ export default function ProjectsSection() {
                 </div>
                 <div className="flex flex-col flex-grow">
                   <div className="flex-grow">
-                    <div className="mt-8 flex items-center gap-x-4 text-xs">
+                    <div className="mt-5 flex items-center justify-between text-xs">
                       <time dateTime={t(`projects.${key}.date`)} className="text-gray-500">
                         {t(`projects.${key}.date`)}
                       </time>
@@ -54,15 +57,18 @@ export default function ProjectsSection() {
                         {t(`projects.${key}.category`)}
                       </span>
                     </div>
+
                     <div className="group relative">
                       <h3 className="mt-3 text-lg text-center font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                         <span className="absolute inset-0" />
                         {t(`projects.${key}.title`)}
                       </h3>
-                      <p className="mt-5 text-sm leading-6 text-gray-600 h-36 overflow-y-auto">{t(`projects.${key}.description`)}</p>
+                      <p className="mt-5 text-sm leading-6 text-gray-600 h-36 overflow-y-auto text-justify">
+                        {t(`projects.${key}.description`)}
+                      </p>
                     </div>
                   </div>
-                  <div className="relative mt-8 flex items-start gap-x-4">
+                  <div className="relative flex items-start gap-x-4">
                     <img src={avatarMap[key]} alt="" className="h-10 w-10 rounded-full bg-gray-100" />
                     <div className="text-sm leading-6">
                       <p className="font-semibold text-gray-900">
