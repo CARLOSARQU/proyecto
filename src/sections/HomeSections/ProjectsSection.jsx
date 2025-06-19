2ff8672b57b41f3a27245271c5449e85b7816d86
RYUSNEY <ruapazap@est.unap.edu.pe>
2025-06-19 11:10:23 -0500

    Padding disminuido

--- src/sections/HomeSections/ProjectsSection.jsx
+++ src/sections/HomeSections/ProjectsSection.jsx
@@ -26,7 +26,7 @@ export default function ProjectsSection() {
   const postKeys = ['a', 'b', 'c', 'd', 'e'];
 
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white py-5 sm:py-5">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{t('projects.title')}</h2>
